bfd9fa196339d8ce812b66a2d27e3ac7fb05574d
joaocarlonexxus-cyber <joao.carlo.nexxus@gmail.com>
2026-09-02 14:59:21 -0300

    insercao-de-sqls

--- sql/projetos/visao-geral/taxa-retrabalho.sql
+++ sql/projetos/visao-geral/taxa-retrabalho.sql
@@ -10,11 +10,11 @@ WITH Projetos AS
         ON proj.porte_projeto = porte.porte_projeto
     WHERE
         proj.status_projeto = 'Finalizado'
-        --[[AND proj.data_abertura_proposta >= {{data_inicial}}]]
-        --[[AND proj.data_abertura_proposta <= {{data_final}}]]
-        --[[AND {{setor}}]]
-        --[[AND {{porte_projeto}}]]
-        --[[AND {{colaborador}}]]
+        [[AND proj.data_abertura_proposta >= {{data_inicial}}]]
+        [[AND proj.data_abertura_proposta <= {{data_final}}]]
+        [[AND {{setor}}]]
+        [[AND {{porte_projeto}}]]
+        [[AND {{colaborador}}]]
     GROUP BY
         proj.task_gid
 )
