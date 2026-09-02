237838eaf37feec85f60b964119ef3747fb6e9fb
joaocarlonexxus-cyber <joao.carlo.nexxus@gmail.com>
2026-09-02 15:46:28 -0300

    insercao-de-sqls

---
+++ sql/projetos/desempenho/horas-sem-estimativa-desenv.sql
@@ -0,0 +1,27 @@
+WITH Projetos AS
+(
+    SELECT
+        proj.task_gid,
+        MAX(proj.tempo_real_desenv_geral) AS tempo_real_desenv_geral
+    FROM dbo.vw_projetos_tratada AS proj
+    LEFT JOIN dbo.d_colaboradores AS colab
+        ON proj.colaborador = colab.colaborador
+    LEFT JOIN dbo.d_porte_projeto AS porte
+        ON proj.porte_projeto = porte.porte_projeto
+    WHERE
+        (
+            proj.tempo_estimado_desenv_geral IS NULL
+            OR proj.tempo_estimado_desenv_geral = 0
+        )
+        AND proj.tempo_real_desenv_geral > 0
+        --[[AND proj.data_abertura_proposta >= {{data_inicial}}]]
+        --[[AND proj.data_abertura_proposta <= {{data_final}}]]
+        --[[AND {{setor}}]]
+        --[[AND {{porte_projeto}}]]
+        --[[AND {{colaborador}}]]
+    GROUP BY
+        proj.task_gid
+)
+SELECT
+    COALESCE(SUM(tempo_real_desenv_geral), 0) AS [Horas sem Estimativa - Desenv.]
+FROM Projetos;
---
+++ sql/projetos/desempenho/projetos-sem-estimativa-desenv.sql
@@ -0,0 +1,18 @@
+SELECT
+    COUNT(DISTINCT proj.task_gid) AS [Total de Projetos]
+FROM dbo.vw_projetos_tratada AS proj
+    LEFT JOIN dbo.d_colaboradores AS colab
+        ON proj.colaborador = colab.colaborador
+    LEFT JOIN dbo.d_porte_projeto AS porte
+        ON proj.porte_projeto = porte.porte_projeto
+WHERE
+    (
+        proj.tempo_estimado_desenv_geral IS NULL
+        OR proj.tempo_estimado_desenv_geral = 0
+    )
+    AND proj.tempo_real_desenv_geral > 0
+    --[[AND proj.data_abertura_proposta >= {{data_inicial}}]]
+    --[[AND proj.data_abertura_proposta <= {{data_final}}]]
+    --[[AND {{setor}}]]
+    --[[AND {{porte_projeto}}]]
+    --[[AND {{colaborador}}]];
