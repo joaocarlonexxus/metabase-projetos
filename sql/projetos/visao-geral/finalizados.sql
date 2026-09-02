00a4c86a9c2c0319f32f13c880ed68ec3a75ad29
joaocarlonexxus-cyber <joao.carlo.nexxus@gmail.com>
2026-09-02 14:01:24 -0300

    insercao-de-sqls

---
+++ sql/projetos/visao-geral/finalizados.sql
@@ -0,0 +1,14 @@
+SELECT
+    COUNT(DISTINCT proj.task_gid) AS [Total de Projetos]
+FROM dbo.vw_projetos_tratada AS proj
+	LEFT JOIN dbo.d_colaboradores AS colab
+    	ON proj.colaborador = colab.colaborador
+	LEFT JOIN dbo.d_porte_projeto AS porte
+    	ON proj.porte_projeto = porte.porte_projeto
+WHERE
+    proj.status_projeto = 'Finalizado'
+    [[AND proj.data_abertura_proposta >= {{data_inicial}}]]
+    [[AND proj.data_abertura_proposta <= {{data_final}}]]
+    [[AND {{setor}}]]
+    [[AND {{porte_projeto}}]]
+    [[AND {{colaborador}}]];
