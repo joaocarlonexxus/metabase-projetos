abbf88ed3e30bd8a44829060e6f7b226dbec8665
joaocarlonexxus-cyber <joao.carlo.nexxus@gmail.com>
2026-09-11 13:29:27 -0300

    ajustes-scripts

--- sql/projetos/visao-geral/horas-itens-fora-escopo.sql
+++ sql/projetos/visao-geral/horas-itens-fora-escopo.sql
@@ -1,23 +1,40 @@
 WITH Projetos AS
 (
-    SELECT
-        proj.task_gid,
-        MAX(proj.horas_itens_fora_escopo) AS horas_itens_fora_escopo
-    FROM dbo.vw_projetos_tratada AS proj
-	    LEFT JOIN dbo.d_colaboradores AS colab
-	        ON proj.colaborador = colab.colaborador
-	    LEFT JOIN dbo.d_porte_projeto AS porte
-	        ON proj.porte_projeto = porte.porte_projeto
-    WHERE
-        proj.status_projeto = 'Finalizado'
-        [[AND proj.data_termino >= {{data_inicial}}]]
-        [[AND proj.data_termino <= {{data_final}}]]
-        [[AND {{setor}}]]
-        [[AND {{porte_projeto}}]]
-        [[AND {{colaborador}}]]
-    GROUP BY
-        proj.task_gid
+	SELECT
+		proj.task_gid,
+		MAX(proj.horas_itens_fora_escopo) AS horas_itens_fora_escopo
+	FROM dbo.vw_projetos_tratada AS proj
+	LEFT JOIN dbo.d_porte_projeto AS porte
+		ON proj.porte_projeto = porte.porte_projeto
+	WHERE proj.status_projeto = 'Finalizado'
+		[[AND proj.data_termino >= {{data_inicial}}]]
+		[[AND proj.data_termino <= {{data_final}}]]
+		[[
+		AND EXISTS
+		(
+			SELECT 1
+			FROM dbo.b_projetos_setores AS bp_setor
+			INNER JOIN dbo.d_setores AS st
+				ON bp_setor.setor_id = st.setor_id
+			WHERE bp_setor.task_gid = proj.task_gid
+				AND {{setor}}
+		)
+		]]
+		[[AND {{porte_projeto}}]]
+		[[
+		AND EXISTS
+		(
+			SELECT 1
+			FROM dbo.b_projetos_colaboradores AS bp_colab
+			INNER JOIN dbo.d_colaboradores AS colab
+				ON bp_colab.colaborador_id = colab.colaborador_id
+			WHERE bp_colab.task_gid = proj.task_gid
+				AND {{colaborador}}
+		)
+		]]
+	GROUP BY
+		proj.task_gid
 )
 SELECT
-    COALESCE(SUM(horas_itens_fora_escopo), 0) AS [Horas de Itens Fora do Escopo]
+	COALESCE(SUM(horas_itens_fora_escopo), 0) AS [Horas de Itens Fora do Escopo]
 FROM Projetos;
--- sql/projetos/visao-geral/projetos.ativos.sql
+++ sql/projetos/visao-geral/projetos.ativos.sql
@@ -1,26 +1,43 @@
 SELECT
-    COUNT(DISTINCT proj.task_gid) AS [Total de Projetos]
+	COUNT(DISTINCT proj.task_gid) AS [Total de Projetos]
 FROM dbo.vw_projetos_tratada AS proj
-    LEFT JOIN dbo.d_colaboradores AS colab
-        ON proj.colaborador = colab.colaborador
-    LEFT JOIN dbo.d_porte_projeto AS porte
-        ON proj.porte_projeto = porte.porte_projeto
-WHERE
-    proj.status_projeto IN (
-        'Em Desenvolvimento',
-        'Em Implantação',
-        'Fornecimento Materiais',
-        'Em Fechamento'
-    )
-    AND proj.task_section_name NOT IN (
-        '11. Controle Suporte',
-        '12. Controle de Suporte - Vencidos',
-        '13. Reunião de Fechamento',
-        '14. Finalizado',
-        '15. Excedente de Horas de Suporte'
-    )
-    [[AND proj.data_abertura_proposta >= {{data_inicial}}]]
-    [[AND proj.data_abertura_proposta <= {{data_final}}]]
-    [[AND {{setor}}]]
-    [[AND {{porte_projeto}}]]
-    [[AND {{colaborador}}]];
+LEFT JOIN dbo.d_porte_projeto AS porte
+	ON proj.porte_projeto = porte.porte_projeto
+WHERE proj.status_projeto IN (
+		'Em Desenvolvimento',
+		'Em Implantação',
+		'Fornecimento Materiais',
+		'Em Fechamento'
+	)
+	AND proj.task_section_name NOT IN (
+		'11. Controle Suporte',
+		'12. Controle de Suporte - Vencidos',
+		'13. Reunião de Fechamento',
+		'14. Finalizado',
+		'15. Excedente de Horas de Suporte'
+	)
+	[[AND proj.data_abertura_proposta >= {{data_inicial}}]]
+	[[AND proj.data_abertura_proposta <= {{data_final}}]]
+	[[
+	AND EXISTS
+	(
+		SELECT 1
+		FROM dbo.b_projetos_setores AS bp_setor
+		INNER JOIN dbo.d_setores AS st
+			ON bp_setor.setor_id = st.setor_id
+		WHERE bp_setor.task_gid = proj.task_gid
+			AND {{setor}}
+	)
+	]]
+	[[AND {{porte_projeto}}]]
+	[[
+	AND EXISTS
+	(
+		SELECT 1
+		FROM dbo.b_projetos_colaboradores AS bp_colab
+		INNER JOIN dbo.d_colaboradores AS colab
+			ON bp_colab.colaborador_id = colab.colaborador_id
+		WHERE bp_colab.task_gid = proj.task_gid
+			AND {{colaborador}}
+	)
+	]];
